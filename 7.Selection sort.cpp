095c3f0ad859e777ebe1f19069dc2bc375c82cc6
vishnusanjeev <125140025+vishnusanjeev@users.noreply.github.com>
2024-07-18 14:03:12 +0530

    Add files via upload

---
+++ 7.Selection sort.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+void selection_sort(int arr[], int n) {
+    int i, j, min_idx, temp;
+    for (i = 0; i < n - 1; i++) {
+        min_idx = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+}
+void print_array(int arr[], int size) {
+    int i;
+    for (i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+printf("G.Vishnu Sanjeev (192210101)\n");
+    int n, i;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter the elements: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    selection_sort(arr, n);
+    printf("Sorted array: ");
+    print_array(arr, n);
+
+    return 0;
+}
