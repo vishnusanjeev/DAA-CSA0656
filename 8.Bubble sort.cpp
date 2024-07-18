d858c4ce4d05e46cbef0a8d3d493b10df3e65574
vishnusanjeev <125140025+vishnusanjeev@users.noreply.github.com>
2024-07-18 14:06:32 +0530

    Add files via upload

---
+++ 8.Bubble sort.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+void bubble_sort(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+void print_array(int arr[], int size) {
+    int i;
+    for (i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+printf("G.Vishnu Sanjeev (192210101)\n");
+    int n, i;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    bubble_sort(arr, n);
+    printf("Sorted array: ");
+    print_array(arr, n);
+    return 0;
+}
